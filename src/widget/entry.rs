558e7f3a5cde4e775d8d81ea8eb44effcf317f4a
Bruno Dutra <brunocodutra@gmail.com>
2019-03-24 17:19:43 +0100

    define Entry

---
+++ src/widget/entry.rs
@@ -0,0 +1,57 @@
+#[cfg(test)]
+use proptest_derive::Arbitrary;
+
+/// The semantic representation of text input.
+#[derive(derivative::Derivative)]
+#[derivative(
+    Debug(bound = ""),
+    Default(bound = ""),
+    Clone(bound = ""),
+    Eq(bound = ""),
+    PartialEq(bound = ""),
+    Hash(bound = "")
+)]
+#[cfg_attr(test, derive(Arbitrary))]
+#[cfg_attr(test, proptest(no_bound))]
+pub struct Entry<A: 'static> {
+    pub value: String,
+
+    #[cfg_attr(test, proptest(value = "None"))]
+    pub on_change: Option<fn(String) -> A>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mock::*;
+    use proptest::prelude::*;
+    use std::hash::{Hash, Hasher};
+
+    #[derive(Debug)]
+    enum Action {}
+
+    #[test]
+    fn default() {
+        assert_eq!(
+            Entry::<Action>::default(),
+            Entry {
+                value: "".into(),
+                on_change: None,
+            }
+        );
+    }
+
+    proptest! {
+        #[test]
+        fn clone(entry: Entry<Action>) {
+            assert_eq!(entry.clone(), entry);
+        }
+
+        #[test]
+        fn hash(entry: Entry<Action>) {
+            let mut hasher = NopHash(0);
+            entry.hash(&mut hasher);
+            assert_eq!(hasher.finish(), 0);
+        }
+    }
+}
