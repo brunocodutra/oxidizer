e507d5203445ec886dd26b277a2aaa89d86e6a00
Bruno Dutra <brunocodutra@gmail.com>
2019-03-24 17:15:06 +0100

    define Button

---
+++ src/widget/button.rs
@@ -0,0 +1,57 @@
+#[cfg(test)]
+use proptest_derive::Arbitrary;
+
+/// The semantic representation of a button.
+#[derive(derivative::Derivative)]
+#[derivative(
+    Debug(bound = ""),
+    Default(bound = ""),
+    Clone(bound = ""),
+    Eq(bound = ""),
+    PartialEq(bound = ""),
+    Hash(bound = "")
+)]
+#[cfg_attr(test, derive(Arbitrary))]
+#[cfg_attr(test, proptest(no_bound))]
+pub struct Button<A: 'static> {
+    pub label: String,
+
+    #[cfg_attr(test, proptest(value = "None"))]
+    pub on_click: Option<fn() -> A>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mock::*;
+    use proptest::prelude::*;
+    use std::hash::{Hash, Hasher};
+
+    #[derive(Debug)]
+    enum Action {}
+
+    #[test]
+    fn default() {
+        assert_eq!(
+            Button::<Action>::default(),
+            Button {
+                label: "".into(),
+                on_click: None,
+            }
+        );
+    }
+
+    proptest! {
+        #[test]
+        fn clone(button: Button<Action>) {
+            assert_eq!(button.clone(), button);
+        }
+
+        #[test]
+        fn hash(button: Button<Action>) {
+            let mut hasher = NopHash(0);
+            button.hash(&mut hasher);
+            assert_eq!(hasher.finish(), 0);
+        }
+    }
+}
