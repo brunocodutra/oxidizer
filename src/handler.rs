2f3410285ac767c9950985af6cf41fcf86f2cbbb
Bruno Dutra <brunocodutra@gmail.com>
2019-09-08 14:37:23 +0200

    remove conversions between Handler types

--- src/handler.rs
+++ src/handler.rs
@@ -102,54 +102,6 @@ where
     }
 }
 
-impl<W, E, A> From<Handler<Widget<A>, E, A>> for Handler<W, E, A>
-where
-    W: Variant<Widget<A>>,
-    E: Kind<Event>,
-{
-    fn from(h: Handler<Widget<A>, E, A>) -> Self {
-        use Handler::*;
-        match h {
-            A(f) => Handler::B(f),
-            B(f) => Handler::B(f),
-            C(f) => Handler::D(f),
-            D(f) => Handler::D(f),
-        }
-    }
-}
-
-impl<W, E, A> From<Handler<W, Event, A>> for Handler<W, E, A>
-where
-    W: Kind<Widget<A>>,
-    E: Variant<Event>,
-{
-    fn from(h: Handler<W, Event, A>) -> Self {
-        use Handler::*;
-        match h {
-            A(f) => Handler::C(f),
-            B(f) => Handler::D(f),
-            C(f) => Handler::C(f),
-            D(f) => Handler::D(f),
-        }
-    }
-}
-
-impl<W, E, A> From<Handler<Widget<A>, Event, A>> for Handler<W, E, A>
-where
-    W: Variant<Widget<A>>,
-    E: Variant<Event>,
-{
-    fn from(h: Handler<Widget<A>, Event, A>) -> Self {
-        use Handler::*;
-        match h {
-            A(f) => Handler::D(f),
-            B(f) => Handler::D(f),
-            C(f) => Handler::D(f),
-            D(f) => Handler::D(f),
-        }
-    }
-}
-
 #[cfg(test)]
 use proptest::{arbitrary::Arbitrary, prelude::*};
 
@@ -237,51 +189,5 @@ mod tests {
         assert_eq!(Handler::<Button<_>, Clicked, _>::from(b), Handler::B(b));
         assert_eq!(Handler::<Button<_>, Clicked, _>::from(c), Handler::C(c));
         assert_eq!(Handler::<Button<_>, Clicked, _>::from(d), Handler::D(d));
-
-        {
-            let w = Handler::<Widget<_>, Clicked, _>::A(b);
-            let x = Handler::<Widget<_>, Clicked, _>::B(b);
-            let y = Handler::<Widget<_>, Clicked, _>::C(d);
-            let z = Handler::<Widget<_>, Clicked, _>::D(d);
-
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(w), Handler::B(b));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(x), Handler::B(b));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(y), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(z), Handler::D(d));
-        }
-
-        {
-            let w = Handler::<Button<_>, Event, _>::A(c);
-            let x = Handler::<Button<_>, Event, _>::B(d);
-            let y = Handler::<Button<_>, Event, _>::C(c);
-            let z = Handler::<Button<_>, Event, _>::D(d);
-
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(w), Handler::C(c));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(x), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(y), Handler::C(c));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(z), Handler::D(d));
-        }
-
-        {
-            let w = Handler::<Widget<_>, Event, _>::A(d);
-            let x = Handler::<Widget<_>, Event, _>::B(d);
-            let y = Handler::<Widget<_>, Event, _>::C(d);
-            let z = Handler::<Widget<_>, Event, _>::D(d);
-
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(w), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(x), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(y), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Clicked, _>::from(z), Handler::D(d));
-
-            assert_eq!(Handler::<Widget<_>, Clicked, _>::from(w), Handler::C(d));
-            assert_eq!(Handler::<Widget<_>, Clicked, _>::from(x), Handler::D(d));
-            assert_eq!(Handler::<Widget<_>, Clicked, _>::from(y), Handler::C(d));
-            assert_eq!(Handler::<Widget<_>, Clicked, _>::from(z), Handler::D(d));
-
-            assert_eq!(Handler::<Button<_>, Event, _>::from(w), Handler::B(d));
-            assert_eq!(Handler::<Button<_>, Event, _>::from(x), Handler::B(d));
-            assert_eq!(Handler::<Button<_>, Event, _>::from(y), Handler::D(d));
-            assert_eq!(Handler::<Button<_>, Event, _>::from(z), Handler::D(d));
-        }
     }
 }
